0a8cb98693df38448da50e72d1a5d1bd0f72fd82
Vlad Bubeniuk <ZayByst@mail.ru>
2020-04-03 03:26:08 +0300

    Rebuild proj

--- web_site_engine/settings.py
+++ web_site_engine/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_K')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'healthy_food'
+    'healthy_food.apps.HealthyFoodConfig'
 ]
 
 MIDDLEWARE = [
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
